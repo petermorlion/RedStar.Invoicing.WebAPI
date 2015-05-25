ef50f6ac2f781855a9425e0f3755a18755954b6a
Peter Morlion <peter.morlion@live.be>
2015-05-25 13:25:40 +0200

    add invoice items to template

--- src/RedStar.Invoicing/wwwroot/modules/invoice-generator.js
+++ src/RedStar.Invoicing/wwwroot/modules/invoice-generator.js
@@ -1,4 +1,18 @@
 ﻿export class InvoiceGenerator {
-    
+    items = [];
 
+    addInvoiceItem() {
+        this.items.push(new InvoiceItem());
+        console.log('added item');
+    }
+}
+
+export class InvoiceItem {
+    amount = 1;
+    description = '';
+    unitPrice = 1;
+
+    getTotal() {
+        return this.unitPrice * this.amount;
+    }
 }
