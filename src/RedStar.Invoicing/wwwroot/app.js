5e2e7c7603d56e62bd628377b8b4132dcb4ff848
Peter Morlion <peter.morlion@live.be>
2015-05-31 16:04:18 +0200

    added invoice settings (empty)

--- src/RedStar.Invoicing/wwwroot/app.js
+++ src/RedStar.Invoicing/wwwroot/app.js
@@ -2,7 +2,8 @@
     configureRouter(config, router){
         config.title = 'Redstar Invoicing';
         config.map([
-          { route: ['','invoice-generator'],  moduleId: './modules/invoice-generator',      nav: true, title:'Invoice Generator' }
+          { route: ['','invoice-generator'],  moduleId: './modules/invoice-generator',      nav: true, title:'Invoice Generator' },
+          { route: ['invoice-settings'],  moduleId: './modules/invoice-settings',      nav: true, title:'Invoice Settings' }
         ]);
 
         this.router = router;
