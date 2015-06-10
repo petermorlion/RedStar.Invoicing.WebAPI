8340e0ff75d963c705da80403455d2092572b5c4
Peter Morlion <peter.morlion@live.be>
2015-06-10 12:48:10 +0200

    get method to retrieve invoicetemplate

--- src/RedStar.Invoicing/Controllers/InvoiceController.cs
+++ src/RedStar.Invoicing/Controllers/InvoiceController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNet.Mvc;
 using System.IO;
 using RedStar.Invoicing.Models;
+using System.Security.Claims;
+using System.Linq;
+using Microsoft.AspNet.Authorization;
 
 namespace RedStar.Invoicing.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     public class InvoiceController : Controller
     {
-        // POST api/values
+        private InvoicesDbContext _invoicesDbContext;
+
+        public InvoiceController(InvoicesDbContext invoicesDbContext)
+        {
+            _invoicesDbContext = invoicesDbContext;
+        }
+
+        [HttpGet]
+        public InvoiceGenerationDTO Get()
+        {
+            var userId = Context.User.GetUserId();
+            var userSetting = _invoicesDbContext.UserSettings.SingleOrDefault(x => x.UserId.ToString() == userId);
+
+            return new InvoiceGenerationDTO
+            {
+                InvoiceTemplate = userSetting.InvoiceTemplate,
+                LogoUrl = userSetting.LogoUrl
+            };
+        }
+
         [HttpPost]
         public void Post([FromBody]InvoiceDTO invoice)
         {
---
+++ src/RedStar.Invoicing/Models/InvoiceGenerationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedStar.Invoicing.Models
+{
+    public class InvoiceGenerationDTO
+    {
+        public string InvoiceTemplate { get; set; }
+        public string LogoUrl { get; set; }
+    }
+}
