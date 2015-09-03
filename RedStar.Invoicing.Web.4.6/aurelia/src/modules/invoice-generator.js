d95f328ca335487e0cda5563cfb2304f9ee1a827
Peter Morlion <peter.morlion@live.be>
2015-09-03 19:31:19 +0200

    Added aurelia bits and gulpfile
    Still need to add and init jspm and add Aurelia itsself

---
+++ RedStar.Invoicing.Web.4.6/aurelia/src/modules/invoice-generator.js
@@ -0,0 +1,70 @@
+﻿import {inject} from 'aurelia-framework';
+import {computedFrom} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
+
+@inject(HttpClient)
+export class InvoiceGenerator {
+    items = [];
+    invoiceTemplate = '';
+
+    constructor(http) {
+        this.http = http;
+    }
+    
+    activate() {
+        this.http
+            // TODO: use /api/
+            .get(`http://${window.location.host}/invoicegenerator/logourl`)
+            .then(response => {
+                this.logoUrl = response.content;
+            }).catch(err => {
+                console.log(err); 
+            });
+    }
+
+    addInvoiceItem() {
+        this.items.push(new InvoiceItem());
+    }
+
+    print() {           
+        //TODO: get template from server and then use property instead of DOM
+        let html = document.getElementById('invoice-template').innerHTML;
+        let json = { html : html, invoiceNumber: this.invoiceNumber };
+        
+        this.http.createRequest(`http://${window.location.host}/api/invoice`)
+            .asPost()
+            .withHeader('Content-Type', 'application/json; charset=utf-8')
+            .withContent(json)
+            .send()
+            .then(response => {
+                console.log(response.response);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    get subtotal() {
+        let sum = 0;
+        this.items.map(i => sum += i.total);
+        return sum;
+    }
+
+    get vat() {
+        return 0.21 * this.subtotal;
+    }
+
+    get total() {
+        return this.subtotal + this.vat;
+    }
+}
+
+export class InvoiceItem {
+    amount = 1;
+    description = '';
+    unitPrice = 1;
+
+    @computedFrom('unitPrice', 'amount')
+    get total() {
+        return this.unitPrice * this.amount;
+    }
+}
---
+++ RedStar.Invoicing.Web.4.6/gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require("gulp"),
+   babel = require("gulp-babel")
+      fs = require("fs");
+
+var aureliaSrc = 'aurelia/src';
+var aureliaDist = 'aurelia/dist';
+
+gulp.task("babel", function () {
+    gulp.src([aureliaSrc + '/**/*.js'])
+      .pipe(babel({
+          "optional": ["runtime"],
+          "stage": 0,
+          'modules': 'system'
+      }))
+      .pipe(gulp.dest(aureliaDist));
+});
+
+gulp.task('build-html', function () {
+    gulp.src([aureliaSrc + '/**/*.html'])
+      .pipe(gulp.dest(aureliaDist));
+});
