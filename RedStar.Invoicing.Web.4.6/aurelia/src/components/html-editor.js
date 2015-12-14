1b57307223d5c80dfa5025533032ea90bd10f33b
Peter Morlion <peter.morlion@live.be>
2015-12-14 18:43:08 +0100

    indenting

--- RedStar.Invoicing.Web.4.6/aurelia/src/components/html-editor.js
+++ RedStar.Invoicing.Web.4.6/aurelia/src/components/html-editor.js
@@ -5,33 +5,33 @@ import {customElement, bindable} from 'aurelia-framework';
 export class HtmlEditor {
   @bindable html;
 
-  attached() {
-      var textArea = document.getElementById('invoice-template');
-      var options = {
-          mode: 'htmlmixed',
-          lineNumbers: true,
-          value: this.html
-      };
+    attached() {
+        var textArea = document.getElementById('invoice-template');
+        var options = {
+            mode: 'htmlmixed',
+            lineNumbers: true,
+            value: this.html
+        };
 
-      var codeMirror = fromTextArea(textArea, options);
+        var codeMirror = fromTextArea(textArea, options);
 
-      var vm = this;
+        var vm = this;
 
-      codeMirror.on('change', function(instance, changeObj) {
-          // TODO: use arrow function?
-          vm.html = instance.doc.getValue();
-          console.log('viewmodel updated');
-      });
+        codeMirror.on('change', function(instance, changeObj) {
+            // TODO: use arrow function?
+            vm.html = instance.doc.getValue();
+            console.log('viewmodel updated');
+        });
 
-      this.editor = codeMirror;
-  }
+        this.editor = codeMirror;
+    }
 
-  htmlChanged() {
-      console.log('value changed');
-      if (this.editor && !this.updatedFromAjaxCall)
-      {
-          this.editor.doc.setValue(this.html);
-          this.updatedFromAjaxCall = true;
-      }
-  }
+    htmlChanged() {
+        console.log('value changed');
+        if (this.editor && !this.updatedFromAjaxCall)
+        {
+            this.editor.doc.setValue(this.html);
+            this.updatedFromAjaxCall = true;
+        }
+    }
 }
