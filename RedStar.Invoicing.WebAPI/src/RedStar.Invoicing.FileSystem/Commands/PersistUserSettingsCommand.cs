cbb7e63b8d9e36e1d434c9e15d78217a0bea3441
Peter Morlion <peter.morlion@live.be>
2016-02-29 14:53:47 +0100

    added PersistUserSettingsCommand

---
+++ RedStar.Invoicing.WebAPI/src/RedStar.Invoicing.FileSystem/Commands/PersistUserSettingsCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using RedStar.Invoicing.Commands;
+using RedStar.Invoicing.Domain;
+
+namespace RedStar.Invoicing.FileSystem.Commands
+{
+    public class PersistUserSettingsCommand : IPersistUserSettingsCommand
+    {
+        public Task Execute(UserSettings userSettings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
